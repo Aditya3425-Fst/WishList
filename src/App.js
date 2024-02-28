161e95609bf135a0e72c15bf4ae0a079c6eda699
Aditya kumar Tiwari <sujanian3425@gmail.com>
2024-02-28 10:22:53 +0530

    wishlist

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+import Userinput from './components/UserInput';
+import Data from './components/Data';
+
+function App() {
+  const [wishlist, setWishlist] = useState([]);
+
+  function addToWishlist(newWish) {
+    setWishlist([...wishlist, newWish]);
+  }
+  function clearWishlist() {
+    setWishlist([]);
+  }
+
+  function deleteWish(index) {
+    const newWishlist = [...wishlist];
+    newWishlist.splice(index, 1);
+    setWishlist(newWishlist);
+  }
+
+  return (
+    <div className="wishlist">
+      <Userinput addToWishlist={addToWishlist} clearWishlist={clearWishlist} />
+      <Data wishlist={wishlist} deleteWish={deleteWish} />
+    </div>
+  );
+}
+
+export default App;
